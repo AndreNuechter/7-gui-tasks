3a58b82792a2ed5518f603a1f96a500728db16d4
AN <andre_nuechter@hotmail.de>
2021-11-02 18:32:12 +0100

    Counter test

---
+++ cypress/integration/counter.spec.js
@@ -0,0 +1,25 @@
+describe('Counter', () => {
+    beforeEach(() => {
+        cy.visit(`localhost:3003/counter/`);
+    });
+
+    it('starts at 0', () => {
+        cy.get('input').should('have.value', '0');
+    });
+
+    it('counts one up on button click', () => {
+        cy.get('button').click();
+        cy.get('input').should('have.value', '1');
+    });
+
+    it('displayes a number matching the number of clicks', () => {
+        const number = +(Math.ceil(Math.random() * 20));
+        const button = cy.get('button');
+
+        for (let i = 0; i < number; i += 1) {
+            button.click();
+        }
+
+        cy.get('input').should('have.value', number.toString());
+    });
+});
